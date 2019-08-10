4f1458570eb4f8668cb644a494318d2f81076e3c
fassial <fassial@localhost.localdomain>
2019-08-10 00:24:22 -0700

    source_code/tool: Added mkint/mkshort tool

--- source_code/tool/tool.c
+++ source_code/tool/tool.c
@@ -22,3 +22,35 @@ void *memset(void *dest, unsigned int ch, unsigned int n)
 	return dest;
 }
 
+unsigned int mkint(unsigned short high, unsigned short low)
+{
+	return (high << 16) | low;
+}
+
+unsigned short mkshort(char high, char low)
+{
+	return (high << 8) | low;
+}
+
+unsigned int multiply(unsigned int a, unsigned int b)
+{
+	unsigned int res = 0;
+
+	while (a != 0) {
+		res += b;
+		--a;
+	}
+	return res;
+}
+
+unsigned int division(unsigned int n, unsigned int div)
+{
+	unsigned int res = 0;
+	
+	while (n > div) {
+		n -= div;
+		++res;
+	}
+	return res;
+}
+
--- source_code/tool/tool.h
+++ source_code/tool/tool.h
@@ -1,8 +1,27 @@
 #ifndef _LCORE_TOOL_H
 #define _LCORE_TOOL_H
 
+#define COLOR_BLACK			0
+#define COLOR_RED			4
+#define COLOR_GREEN			2
+#define COLOR_YELLOW		6
+#define COLOR_BLUE			1
+#define COLOR_MAGENTA		5
+#define COLOR_CYAN			3
+#define COLOR_WHITE			7
+
+typedef char * va_list;
+#define _INTSIZEOF(n)		((sizeof(n) + sizeof(unsigned int) - 1) & ~(sizeof(unsigned int) - 1))
+#define va_start(ap, v)		(ap = (va_list) &v + _INTSIZEOF(v))
+#define va_arg(ap, t)		(*(t *)((ap += _INTSIZEOF(t)) - _INTSIZEOF(t)))
+#define va_end(ap)			(ap = (va_list) 0)
+
 extern void *memcpy(void *dest, void *src, unsigned int len);
 extern void *memset(void *dest, unsigned int ch, unsigned int n);
+extern unsigned int mkint(unsigned short high, unsigned short low);
+extern unsigned short mkshort(char high, char low);
+extern unsigned int multiply(unsigned int a, unsigned int b);
+extern unsigned int division(unsigned int n, unsigned int div);
 
 #endif
 
